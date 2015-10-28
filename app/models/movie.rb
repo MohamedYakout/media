0eab536df0cd9f305956335ef10d0c752c60c351
MohamedYakout <mohamed.yakout91@gmail.com>
2015-10-28 21:40:58 +0200

    Update Movie model & Load the data & Save it in DB

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,18 +4,41 @@ class Movie
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :url, type: String
-  field :format, type: String
-  field :width, type: Float
-  field :height, type: Float
-  field :langauge, type: String
-  field :duration, type: Float
-  field :geoLock, type: Mongoid::Boolean
+  field :title, type: String
+  field :description, type: String
+  field :type, type: String, default: "movie"
+  field :publishedDate, type: Integer
+  field :availableDate, type: Integer
 
+  field :metadata, type: Array, default: [{"value": "en", "name": "language"}]
+
+  field :contents, type: Array
+  field :credits, type: Array
+  field :parentalRatings, type: Array
+  field :images, type: Array
+  field :categories, type: Array
+
+  field :media_id, type: String
+
+
+  # The following methods to load data & Save its in DB
   def self.fetch_movies
-	uri = URI.parse("https://demo2697834.mockable.io/movies")
-	response = Net::HTTP.get_response(uri)
-	body = response.body
-	JSON.parse body
+  uri = URI.parse("https://demo2697834.mockable.io/movies")
+  response = Net::HTTP.get_response(uri)
+  body = response.body
+  JSON.parse body
+  end
+
+  def self.add_movies_to_db
+    res_json = Movie.fetch_movies
+
+    arr = res_json["entries"]
+    puts arr
+    arr.each do |mov|
+      movie_id = mov.delete("id")
+      movie = Movie.new(mov)
+      movie.media_id = movie_id
+      movie.save
+    end
   end
 end
