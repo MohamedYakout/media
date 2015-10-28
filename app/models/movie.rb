d5a1fece25fd4bdc61d3fc349ab4e72845487df7
MohamedYakout <mohamed.yakout91@gmail.com>
2015-10-28 19:00:17 +0200

    Add method to fetch movies from URL

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,9 @@
+require 'net/http'
+
 class Movie
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :url, type: String
   field :format, type: String
   field :width, type: Float
@@ -7,4 +11,11 @@ class Movie
   field :langauge, type: String
   field :duration, type: Float
   field :geoLock, type: Mongoid::Boolean
+
+  def self.fetch_movies
+	uri = URI.parse("https://demo2697834.mockable.io/movies")
+	response = Net::HTTP.get_response(uri)
+	body = response.body
+	JSON.parse body
+  end
 end
