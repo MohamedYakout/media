9d3f24124f83683d5e84154327f02819350fb7d3
MohamedYakout <mohamed.yakout91@gmail.com>
2015-10-31 19:38:51 +0200

    Try to fix ActiveRecord issue deployment

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require "active_model/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-require "sprockets/railtie"
 require 'rails/mongoid'
 require 'mongoid'
 
